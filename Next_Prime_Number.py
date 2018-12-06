c2f0a30a5b425715a9b960597a966f04ca4bfd4e
infantcyril <45591857+infantcyril@users.noreply.github.com>
2018-12-06 12:18:38 +0530

    Print next prime number by specifying the position
    This Python program accepts 2 values (only integer values) as input. The 1st value is the current number and the 2nd value is the position of the next prime number that you need. For example if the input is (1,2) the program accepts 1 as current value and returns the prime number residing in the 2nd position next to number 1.

---
+++ Next_Prime_Number.py
@@ -0,0 +1,75 @@
+check_a = 0
+check_b = 0
+while (check_a == 0):
+    try:
+        check_a = 1
+        a = int(input("Enter the value of the Number from where you want to start searching: "))
+        if (a < 0):
+            check_b = 0
+            print("Please enter a Positive Value")
+            continue
+    except ValueError:
+        print("Input should be a Positive Value")
+        check_a = 0
+while(check_b == 0):
+    try:
+        check_b = 1
+        b = int(input("Enter the index value: "))
+        if (b <= 0):
+            check_b = 0
+            print("Enter a positive integer value for b")
+            continue
+    except ValueError:
+        print("Input should be a Positive Value")
+        check_b = 0
+   
+c = []
+while (a <= 0):
+    a += 1
+    break
+def Prime3(a,b,c):
+    for num in range(a+1,((a+50*b))):
+        for i in range(2,num):
+            if(num % i) == 0:
+                break
+        else:
+            c.append(num)
+    print ("The Next Prime Number Residing in The Index Value", b, "is:" , c[b-1])
+Prime3(a,b,c)
+
+'''
+TEST CASE:
+Sample Input :
+(2,5)
+
+Expected Output:
+11
+ -----------------
+
+Sample Input :
+(7,3)
+
+Expected Output:
+17
+------------------
+
+Sample Input :
+(2,100)
+
+Expected Output:
+547
+------------------
+
+Sample Input :
+(1000,5)
+
+Expected Output:
+1031
+------------------
+Sample Input :
+(2,5)
+
+Expected Output:
+11
+
+'''
