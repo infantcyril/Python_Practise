6827630308ecd6ae1fdd340e04b8c5f249e8608c
infantcyril <45591857+infantcyril@users.noreply.github.com>
2018-12-14 11:10:55 +0530

    Update of Prime_Number
    Few Test Cases added & Validation done inside the function

--- Prime_Number.py
+++ Prime_Number.py
@@ -1,23 +1,26 @@
 check_a = 0
-while (check_a == 0):
-    try:
-        check_a = 1
-        a = int(input("Enter the Number from where you want to start searching: "))
-        if (a <= 1):
+a = 0
+def prime(check,a):
+    check_a = 0
+    while (check_a == 0):
+        try:
+            check_a = 1
+            a = int(input("Enter the Number from where you want to start searching: "))
+            if (a <= 1):
+                check_a = 0
+                print("Please enter a value greater than 1, There is no Prime number before", a)
+                continue
+        except ValueError:
+            print("Input should be a Positive Integer Value")
             check_a = 0
-            print("Please enter a value greater than 1, There is no Prime number before", a)
-            continue
-    except ValueError:
-        print("Input should be a Positive Integer Value")
-        check_a = 0
-def prime(a):
-    for num in range(1,a):
-        for i in range(2,num):
-            if(num % i) == 0:
-                break
-        else:
-            print ("The prime number that comes first when we count backwards from", a,"is:" , (max(c)))
-prime(a)
+
+    for i in range(2,a):
+        if(a % i) == 0:
+            print(a,"is not a Prime number")
+            break
+    else:
+        print (a,"is a Prime number")
+prime(check_a,a)
 
 '''
 TEST CASE:
@@ -32,4 +35,8 @@ Sample Input : 1000 || Expected Output: 997
 Sample Input : 2100 || Expected Output: 2111
 
 Sample Input : 10000 || Expected Output: 9973
+
+Sample input: kj<space>sh || Expected Output : Input should be a positive value
+
+Sample Input : 1<sapce>5 || Expected Output: Input should be a positive valu
 '''
