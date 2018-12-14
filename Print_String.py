06b81c73e8c8b6c474725348300bdd01e96413e9
infantcyril <45591857+infantcyril@users.noreply.github.com>
2018-12-14 11:19:31 +0530

    Update Print_String1.1.py
    Program Updated

--- Print_String.py
+++ Print_String.py
@@ -1,8 +1,9 @@
-x = (python -V)
+import sys
+v= (sys.version)
 a = "Hello, World,"
 b = "Python is a wonderful language,"
 c = "I am beginner in Python."
 
-print ("Hi this is",x)
+print ("Hi this is python version",x)
 print (a,b,c)
 
